4f323a0939b4c229e5a28c0bbd4cd31c89cb46e5
Nikita Belyaev <112763479+humanbelnik@users.noreply.github.com>
2022-11-28 20:14:39 +0300

    Add files via upload

---
+++ Final_Labs/Lab_12/Lab_12.py
@@ -0,0 +1,13 @@
+"""
+Беляев Никита
+ИУ7 - 11б
+
+Лабораторная работа №12. "Обработка текста"
+"""
+
+def some_function(variable):
+    return variable ** 2
+
+
+variable = int(input("Укажите значение переменной: "))
+print(some_function(variable))
